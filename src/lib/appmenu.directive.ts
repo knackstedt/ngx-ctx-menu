ed877417a1f746152248cf2ca708d47f55f7ff2d
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-18 17:37:56 -0400

    Localize event fire to tagged element

--- src/lib/appmenu.directive.ts
+++ src/lib/appmenu.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, HostListener, ViewContainerRef } from '@angular/core';
+import { Directive, Input, HostListener, ViewContainerRef, AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { calcMenuItemBounds, ContextMenuComponent } from './context-menu/context-menu.component';
 import { ContextMenuItem } from './types';
@@ -9,12 +9,7 @@ const arrowPadding = 8;
     selector: '[ngx-app-menu]',
     standalone: true
 })
-export class NgxAppMenuDirective {
-
-    /**
-     * The data representing the item the context-menu was opened for.
-     */
-    @Input("ngx-app-menu-context") data: any;
+export class NgxAppMenuDirective implements AfterViewInit {
 
     /**
      * The items that will be bound to the menu.
@@ -22,20 +17,30 @@ export class NgxAppMenuDirective {
     @Input("ngx-app-menu") menuItems: ContextMenuItem[];
 
     /**
+     * The data representing the item the context-menu was opened for.
+     */
+    @Input("ngx-app-menu-context") data: any;
+
+    /**
      *
      */
-    @Input("ngx-app-menu-config") config: {
+    @Input("ngx-app-menu-config") config: Partial<{
         position: "top" | "right" | "bottom" | "left",
         alignment:  "center" | "beforestart" | "start" | "end" | "afterend",
         showArrow: boolean,
         arrowSize: number,
         edgePadding: number
-    };
+    }>;
 
     constructor(
         private dialog: MatDialog,
         private viewContainer: ViewContainerRef
     ) { }
+
+    ngAfterViewInit() {
+        const el = this.viewContainer.element.nativeElement as HTMLElement;
+        el.onclick = this.onClick.bind(this);
+    }
 
     getPosition() {
         const src = (this.viewContainer.element.nativeElement as HTMLElement).getBoundingClientRect();
@@ -50,8 +55,8 @@ export class NgxAppMenuDirective {
             left: null
         };
 
-        if (this.config.position == "left" || this.config.position == "right" || !this.config.position) {
-            switch(this.config.alignment) {
+        if (this.config?.position == "left" || this.config?.position == "right" || !this.config?.position) {
+            switch(this.config?.alignment) {
 
                 case "end": {
                     // vertically bind to bottom
@@ -83,24 +88,24 @@ export class NgxAppMenuDirective {
 
             // Apply bounds to prevent the dialog from being cut-off screen
             // Lower bound
-            cords.top = Math.max(this.config.edgePadding || 0, cords.top);
+            cords.top = Math.max(this.config?.edgePadding || 0, cords.top);
             // Upper bound
             cords.top = Math.min(winh, cords.top);
 
-            if (this.config.position == "left") {
-                cords.left = src.x - (width + this.config.arrowSize + arrowPadding);
+            if (this.config?.position == "left") {
+                cords.left = src.x - (width + (this.config?.arrowSize || 0) + arrowPadding);
             }
-            if (this.config.position == "right" || !this.config.position) {
-                cords.left = src.x + (src.width + this.config.arrowSize + arrowPadding);
+            if (this.config?.position == "right" || !this.config?.position) {
+                cords.left = src.x + (src.width + (this.config?.arrowSize || 0) + arrowPadding);
             }
 
             // Lower bound
-            cords.left = Math.max(this.config.edgePadding || 0, cords.left);
+            cords.left = Math.max(this.config?.edgePadding || 0, cords.left);
             // Upper bound
             cords.left = Math.min(winw, cords.left);
         }
-        else if (this.config.position == "top" || this.config.position == "bottom") {
-            switch (this.config.alignment) {
+        else if (this.config?.position == "top" || this.config?.position == "bottom") {
+            switch (this.config?.alignment) {
                 case "end": {
                     // vertically bind to right
                     cords.left = src.x + src.width - width;
@@ -131,20 +136,20 @@ export class NgxAppMenuDirective {
 
             // Apply bounds to prevent the dialog from being cut-off screen
             // Lower bound
-            cords.left = Math.max(this.config.edgePadding || 0, cords.left);
+            cords.left = Math.max(this.config?.edgePadding || 0, cords.left);
             // Upper bound
             cords.left = Math.min(winw, cords.left);
 
 
-            if (this.config.position == "top") {
-                cords.top = src.y - (height + this.config.arrowSize + arrowPadding);
+            if (this.config?.position == "top") {
+                cords.top = src.y - (height + (this.config?.arrowSize || 0) + arrowPadding);
             }
-            if (this.config.position == "bottom") {
-                cords.top = src.y + (src.height + this.config.arrowSize + arrowPadding);
+            if (this.config?.position == "bottom") {
+                cords.top = src.y + (src.height + (this.config?.arrowSize || 0) + arrowPadding);
             }
 
             // Lower bound
-            cords.top = Math.max(this.config.edgePadding || 0, cords.top);
+            cords.top = Math.max(this.config?.edgePadding || 0, cords.top);
             // Upper bound
             cords.top = Math.min(winh, cords.top);
         }
@@ -153,17 +158,13 @@ export class NgxAppMenuDirective {
         cords.top = cords.top + 'px';
         cords.left = cords.left + 'px';
 
+        console.log("Coordinates", cords, {width, height, winh, winw})
 
         return cords;
     }
 
     // Needs to be public so we can manually open the dialog
-    @HostListener('click', ['$event'])
-    @HostListener('touch', ['$event'])
-    public onContextMenu(evt: MouseEvent) {
-        evt.preventDefault();
-        evt.stopPropagation();
-
+    private onClick(evt: MouseEvent) {
         const cords = this.getPosition();
 
         // Create the context menu
